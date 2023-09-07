f05e91ab54e58a9285d5574443304e661be1c3b4
suraj sakhare <sakharesuraj10@gmail.com>
2023-09-07 22:25:24 +0530

    linear search

--- day5.cpp
+++ day5.cpp
@@ -1,6 +1,43 @@
- #include <iostream>
- using namespace std;
- int main(){
-    cout<<"to be continue..";
-    return 0;
+#include <iostream>
+using namespace std;
+
+// Linear search 
+// if found location is i , otherwise return -1
+int search(string arr[], int n, string x)
+{
+	int i;
+	for (i = 0; i < n; i++){
+		if (arr[i] == x){
+			return i;
+            }
+            }
+	return -1;
+}
+
+// searching  string
+int main()
+{
+    int n;
+    cout<<"length of arr : ";
+    cin>>n;
+	string arr[n];
+    cout<<"your string array : ";
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+	string x;
+    cout<<"search for arr : ";
+    cin>>x;
+	int index = search(arr, n, x);
+	if (index == -1){
+		cout << "Element is not present in the array";
+        }
+	else{
+		cout << "Element found at position " << index;
+    }
+	return 0;
 }
