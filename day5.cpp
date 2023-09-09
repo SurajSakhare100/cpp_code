1dd47b2626aacb40f86aa52c7f71ef488867189e
suraj sakhare <sakharesuraj10@gmail.com>
2023-09-09 21:00:16 +0530

    searching

--- day5.cpp
+++ day5.cpp
@@ -1,43 +1,94 @@
 #include <iostream>
 using namespace std;
 
-// Linear search 
-// if found location is i , otherwise return -1
-int search(string arr[], int n, string x)
-{
-	int i;
-	for (i = 0; i < n; i++){
-		if (arr[i] == x){
-			return i;
-            }
-            }
-	return -1;
-}
+// Linear search by function
+// if found , then return location i , otherwise return -1
 
-// searching  string
+
+// int linearSearch(string arr[], int n, string x)
+// {
+// 	int i;
+// 	for (i = 0; i < n; i++){
+// 		if (arr[i] == x){
+// 			return i;
+//             }
+//             }
+// 	return -1;
+// }
+
+// binary search using function
+
+// int binarySearch(int arr[],int size,int key){
+//     int low =0;
+//     int high=size-1;
+//     int mid=(low +high)/2;
+//     while(low<=high){
+//         if (arr[mid]==key){
+//             return mid;
+//         }
+//         //left side
+//         if(key>arr[mid]){
+//             low=mid+1;
+//         }
+//         else{ 
+//         //right side
+//            high=mid-1;
+//         }
+//           mid=(low +high)/2;
+//     }
+//     return -1;
+// }
 int main()
 {
-    int n;
-    cout<<"length of arr : ";
-    cin>>n;
-	string arr[n];
-    cout<<"your string array : ";
-    for (int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    for (int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-	string x;
-    cout<<"search for arr : ";
-    cin>>x;
-	int index = search(arr, n, x);
-	if (index == -1){
-		cout << "Element is not present in the array";
-        }
-	else{
-		cout << "Element found at position " << index;
-    }
+    // ***** Linear search *****
+    // int n;
+    // cout<<"length of arr : ";
+    // cin>>n;
+	// string arr[n];
+    // cout<<"your string array : ";
+    // for (int i=0;i<n;i++){
+    //     cin>>arr[i];
+    // }
+    // for (int i=0;i<n;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+    // cout<<endl;
+	// string x;
+    // cout<<"search for arr : ";
+    // cin>>x;
+	// int index = linearSearch(arr, n, x);
+	// if (index == -1){
+	// 	cout << "Element is not present in array";
+    //     }
+	// else{
+	// 	cout << "Element found at position " << index;
+    // }
+    
+
+    // ****** binery search ****
+
+    // int size;
+    // cout<<"size of array: ";
+    // cin >>size;
+    // int arr[size];
+    // for(int i =0;i<size;i++){
+    //     cin>>arr[i];
+    // }
+    // cout<<"your of array: ";
+    // for(int i =0;i<size;i++){
+    //     cout<<arr[i]<< " ";
+    // }
+    // cout<<endl;
+    // int key;
+    // cout<<"enter your key: ";
+    // cin>>key;
+    // int array=binarySearch(arr,7,key);
+    // if(array==-1){
+    // cout<<"your key is not present"<<endl;
+    // }
+    // else{
+    //     cout<<"index of key is: "<<array<<endl;
+    // }
+
 	return 0;
 }
